a54a2ab851e208beac55b189ce8369a62a76913d
Bryan Fullam <bryanfullam@gmail.com>
2015-10-22 14:30:03 -0700

    testing
    only remaining issue is to alter validity check to work for
    international format (+1xxxxxxxxxx) instead of domestic (xxxxxxxxxx)

--- sendSMS.php
+++ sendSMS.php
@@ -1,33 +1,43 @@
 <?php
 require_once('/home/occprogr/public_html/vendors/twilio-php-master/Services/Twilio.php'); // Loads twilio helper library
 
-function sendSMS($listOfPhones, $message){
+function sendSMS($listOfPhones){
  	//set API token vars
-	$account_sid = 'ACc7ac0e71cf8da9cf5a53ad9af9866e96'; 
-	//$auth_token = 'c6c816f5c5203e618205cbb0f25d5435'; 
-	$client = new Services_Twilio($account_sid, $auth_token); 
- 	
- 	for ($listOfPhones as $name => $phone)
+	$account_sid = 'ACc7ac0e71cf8da9cf5a53ad9af9866e96';
+	//$auth_token = 'c6c816f5c5203e618205cbb0f25d5435';
+	$client = new Services_Twilio($account_sid, $auth_token);
+
+ 	foreach ($listOfPhones as $name => $phone)
  	{
  		if (checkIfValidPhone($phone))
  		{
 	 		//use helper library to send message
-			$client->account->messages->create(array( 
-				'To' => $phone, 
-				'From' => "+14243226078", 
-				'Body' => createMessage($name), 
-			));
+			// $client->account->messages->create(array(
+			// 	'To' => $phone,
+			// 	'From' => "+14243226078",
+			// 	'Body' => createMessage($name),
+			//));
+			echo "TESTING: sending message: " . createMessage($name) . " to " . $phone;
+		}
+		else {
+			echo "TESTING: Phone number invalid";
 		}
 	}
 }
 
-function checkIfValidPhone($phone) 
+function checkIfValidPhone($phone)
 {
-	return (preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $phone));
+	return (preg_match("/^[0-9]{11}$/", $phone));
 }
 
 function createMessage($name)
 {
-	return $name + ", this is a reminder that pogramming club begins in 15 minutes.";
+	return $name . ", this is a reminder that pogramming club begins in 15 minutes.";
 }
+
+$listOfPeople = array(
+	'Bryan' => '17146558300'
+	);
+
+sendSMS($listOfPeople);
 ?>
