1daeaee16fd080294f41beb9aff1a83befb6b0c5
Bryan Fullam <bryanfullam@gmail.com>
2015-10-28 22:28:02 -0700

    relative url and quote bug fix
    made url into relative url for security, and fixed weird quote bug by
    turning single quotes to double quotes… probably unnecessary.

--- sendSMS.php
+++ sendSMS.php
@@ -1,10 +1,10 @@
 <?php
-require_once('.../twilio-php-master/Services/Twilio.php'); // Loads twilio helper library
+require_once('vendors/twilio-php-master/Services/Twilio.php'); // Loads twilio helper library
 
 function sendSMS($listOfPhones){
  	//set API token vars
-	$account_sid = 'ACCOUNT SID HERE';
-	//$auth_token = 'AUTH TOKEN HERE';
+	$account_sid = "12345";
+	$auth_token = "12345";
 	$client = new Services_Twilio($account_sid, $auth_token);
 
  	foreach ($listOfPhones as $member)
@@ -15,13 +15,13 @@ function sendSMS($listOfPhones){
  		{
 	 		//use helper library to send message
 			// $client->account->messages->create(array(
-			 	//'To' => '+' . $phone,
-			 	//'From' => "+14243226078",
-			 	//'Body' => createMessage($name),
+			 	//"To" => '+' . $phone,
+			 	//"From" => "+14243226078",
+			 	//"Body" => createMessage($name),
 			//));
 
 
-			echo "TESTING: sending message: " . createMessage($name) . " to " . "+" . $phone;
+			echo "TESTING: sending message: " . createMessage($name) . " to " . "+" . $phone . " ";
 		}
 		else {
 			echo "TESTING: Phone number invalid";
@@ -46,17 +46,16 @@ function createMessage($name)
 //test array and function call
 $clubMemberList = array(
     array(
-        'name' => 'Georgy',
-        'phone' => '7140000000',
-        'extraInfoInTheFuture' => 'bla bla'
+        "name" => 'Georgy',
+        "phone" => '17140000000',
+        "extraInfoInTheFuture" => 'bla bla'
     ),
     array(
-        'name' => 'Bryan',
-        'phone' => '7140000001',
-        'extraInfoInTheFuture' => 'bla bla'
+        "name" => "Bryan",
+        "phone" => "17140000001",
+        "extraInfoInTheFuture" => "bla bla"
     )
-	)
 );
 
-sendSMS($listOfPeople);
+sendSMS($clubMemberList);
 ?>
